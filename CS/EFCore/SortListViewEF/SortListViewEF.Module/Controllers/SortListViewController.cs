9b73030c09294e0b8c8b6baa12fcc649fc260306
andrey <andrey.kozhevnikov@devexpress.com>
2024-04-26 16:10:41 +0400

    rem hardcode names

--- CS/EFCore/SortListViewEF/SortListViewEF.Module/Controllers/SortListViewController.cs
+++ CS/EFCore/SortListViewEF/SortListViewEF.Module/Controllers/SortListViewController.cs
@@ -6,24 +6,24 @@ using DevExpress.ExpressApp.Model;
 using SortListView.Module.BusinessObjects;
 
 namespace WinWebSolution.Module {
-    public abstract class SortListViewControllerBase : ViewController<ListView> {
+    public abstract class SortListViewControllerBase :ViewController<ListView> {
         public SortListViewControllerBase() {
             TargetObjectType = typeof(Issue);
         }
         protected override void OnActivated() {
             base.OnActivated();
-            string propertyName = "ModifiedOn";
+            string propertyName = nameof(Issue.ModifiedOn);
             bool demoFlag = true;
             //Dennis: This code applies a client side sorting.
-            if (demoFlag) {
-                IModelColumn columnInfo = ((IModelList<IModelColumn>)View.Model.Columns)[propertyName];
-                if (columnInfo != null) {
+            if(demoFlag) {
+                IModelColumn columnInfo = View.Model.Columns[propertyName];
+                if(columnInfo != null) {
                     columnInfo.SortIndex = 0;
                     columnInfo.SortOrder = ColumnSortOrder.Descending;
                 }
             } else {
                 //Dennis: This code is used for the server side sorting.
-                if (((IModelList<IModelSortProperty>)View.Model.Sorting)[propertyName] == null) {
+                if(View.Model.Sorting[propertyName] == null) {
                     IModelSortProperty sortProperty = View.Model.Sorting.AddNode<IModelSortProperty>(propertyName);
                     sortProperty.Direction = SortingDirection.Descending;
                     sortProperty.PropertyName = propertyName;
