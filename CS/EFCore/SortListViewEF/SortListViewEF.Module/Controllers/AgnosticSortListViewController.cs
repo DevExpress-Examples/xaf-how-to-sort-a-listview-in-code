55dd6018f06f9b5d995816898d3800d2ba62d868
Manuel Sanchez <manuel.sanchez@devexpress.com>
2025-11-07 14:05:50 +0100

    Replaced old win/Blazor Controllers with a new platform agnostic controller

---
+++ CS/EFCore/SortListViewEF/SortListViewEF.Module/Controllers/AgnosticSortListViewController.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.ExpressApp.Editors;
+using SortListView.Module;
+
+namespace SortListViewEF.Module.Controllers
+{
+    public partial class AgnosticSortListViewController : SortListViewControllerBase
+    {
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            if (View.Editor is ColumnsListEditor listEditor)
+            {
+                foreach (var column in listEditor.Columns)
+                {
+                    column.AllowSortingChange = false;
+                }
+            }
+        }
+    }
+}
